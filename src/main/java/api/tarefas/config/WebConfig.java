6ca2e2736bd500f405a6d99a4db5bba86ae233ae
HenriquePedro2905 <136708570+HenriquePedro2905@users.noreply.github.com>
2024-07-25 17:37:01 -0300

    testeWebConfig.java

--- src/main/java/api/tarefas/config/WebConfig.java
+++ src/main/java/api/tarefas/config/WebConfig.java
@@ -1,23 +1,23 @@
-package api.tarefas.config;
+// package api.tarefas.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500", "https://front-tarefas-git-master-pedro-henriques-projects-38461c5c.vercel.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
-}
+// @Configuration
+// public class WebConfig {
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurer() {
+//             @Override
+//             public void addCorsMappings(CorsRegistry registry) {
+//                 registry.addMapping("/**")
+//                         .allowedOrigins("http://127.0.0.1:5500", "https://front-tarefas-git-master-pedro-henriques-projects-38461c5c.vercel.app")
+//                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                         .allowedHeaders("*")
+//                         .allowCredentials(true);
+//             }
+//         };
+//     }
+// }
